2e7dc451b1727893372d92c09eab81f9b6c98dc9
renant <renan_replay@live.com>
2019-05-04 15:52:49 -0300

    buscando repositorios e adicionando a lista

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,53 @@
-import React from 'react';
+import moment from 'moment';
+import React, { Component } from 'react';
+import api from '../../services/api';
 import logo from '../../assets/logo.png';
 
 import { Container, Form } from './styles';
 
 import CompareList from '../../components/CompareList';
 
-const Main = () => (
-  <Container>
-    <img src={logo} alt="Github Compare" />
-    <Form>
-      <input type="text" placeholder="usuário/repositório" />
-      <button type="submit">OK</button>
-    </Form>
-    <CompareList />
-  </Container>
-);
+export default class Main extends Component {
+  state = {
+    repositoryInput: '',
+    repositories: [],
+  };
 
-export default Main;
+  handleAddRepository = async (e) => {
+    e.preventDefault();
+    const { repositoryInput, repositories } = this.state;
+
+    try {
+      const { data: repository } = await api.get(`repos/${repositoryInput}`);
+
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
+
+      this.setState({
+        repositoryInput: '',
+        repositories: [...repositories, repository],
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  render() {
+    const { repositories, repositoryInput } = this.state;
+
+    return (
+      <Container>
+        <img src={logo} alt="Github Compare" />
+        <Form onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuário/repositório"
+            value={repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">OK</button>
+        </Form>
+        <CompareList repositories={repositories} />
+      </Container>
+    );
+  }
+}
